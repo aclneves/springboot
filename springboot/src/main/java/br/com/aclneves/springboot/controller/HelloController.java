5c819738d65699d19807107e6df6f14b55fc1357
Antonio Neves <aclneves@gmail.com>
2021-06-26 01:51:16 -0300

    Criação do pacote Controller e da classe HelloController

---
+++ springboot/src/main/java/br/com/aclneves/springboot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package br.com.aclneves.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+
+    @GetMapping("/")
+    public String HelloMessage() {
+        return "Hello Digital Innovation One!";
+    }
+
+}
